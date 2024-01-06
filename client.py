55f5fdb27de4d08e8c459fae9b8efc104f04a0af
Mohitsaroya <140790146+Mohitsaroya@users.noreply.github.com>
2024-01-06 11:41:48 -0700

    Updated client.py
    getting the basic code up

---
+++ client.py
@@ -0,0 +1,14 @@
+import asyncio
+import websockets
+
+async def send_message():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        while True:
+            message = input("Enter message: ")
+            await websocket.send(message)
+            response = await websocket.recv()
+            print(f"Server response: {response}")
+
+asyncio.get_event_loop().run_until_complete(send_message())
+#just trying to get the structure up
