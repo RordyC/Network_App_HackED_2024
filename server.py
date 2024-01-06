7ffe2ac8f1574d7ba1edfc6b43b592400b810084
Mohitsaroya <140790146+Mohitsaroya@users.noreply.github.com>
2024-01-06 16:45:44 -0700

    Update server.py
    i realized i was working on my own old code, i changed it

--- server.py
+++ server.py
@@ -1,25 +1,49 @@
-import asyncio
-import websockets
+import socket
+import threading
 
 class ChatServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen()
+        self.clients = []
+        self.aliases = []
 
-    async def handle_client(self, websocket, path):
-        try:
-            while True:
-                message = await websocket.recv()
-                print(f"Received message: {message}")
-                await websocket.send(f"Echo: {message}")
-        except websockets.exceptions.ConnectionClosedError:
-            print("Connection closed")
+    def broadcast(self, message):
+        for client in self.clients:
+            client.send(message)
 
-    def start(self):
-        start_server = websockets.serve(self.handle_client, self.host, self.port)
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
+    def handle_client(self, client):
+        while True:
+            try:
+                message = client.recv(1024)
+                self.broadcast(message)
+            except:
+                index = self.clients.index(client)
+                self.clients.remove(client)
+                client.close()
+                alias = self.aliases[index]
+                self.broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
+                self.aliases.remove(alias)
+                break
+
+    def receive(self):
+        while True:
+            print('Server is running and listening...')
+            client, address = self.server.accept()
+            print(f'Connection is established with {str(address)}')
+            client.send('alias?'.encode('utf-8'))
+            alias = client.recv(1024)
+            self.aliases.append(alias)
+            self.clients.append(client)
+            print(f'The alias of this client is {alias}'.encode('utf-8'))
+            self.broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+            client.send('You are now connected!'.encode('utf-8'))
+            thread = threading.Thread(target=self.handle_client, args=(client,))
+            thread.start()
 
 if __name__ == "__main__":
-    chat_server = ChatServer("localhost", 8001)
-    chat_server.start()
+    chat_server = ChatServer('127.0.0.1', 59000)
+    chat_server.receive()
