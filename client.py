3bc4adf6b4e3b157553933463d8a2543e27e7245
Mohitsaroya <140790146+Mohitsaroya@users.noreply.github.com>
2024-01-06 16:58:56 -0700

    Update client.py
    basic ui

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import threading
 import socket
+import tkinter as tk
+
 
 class ChatClient:
     def __init__(self, host, port):
@@ -8,30 +10,44 @@ class ChatClient:
         self.client.connect((host, port))
         self.client.send(self.alias.encode('utf-8'))
 
+        self.root = tk.Tk()
+        self.root.title('Chat Client')
+
+        self.chat_box = tk.Text(self.root, height=50, width=50)
+        self.chat_box.pack()
+
+        self.message_entry = tk.Entry(self.root, width=50)
+        self.message_entry.pack()
+
+        self.send_button = tk.Button(self.root, text='Send', command=self.send_message)
+        self.send_button.pack()
+
         self.receive_thread = threading.Thread(target=self.receive_message)
-        self.send_thread = threading.Thread(target=self.send_message)
+        self.receive_thread.start()
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def start(self):
-        self.receive_thread.start()
-        self.send_thread.start()
+        self.root.mainloop()
 
     def receive_message(self):
         while True:
             try:
                 message = self.client.recv(1024).decode('utf-8')
-                if message == "alias?":
-                    self.client.send(self.alias.encode('utf-8'))
-                else:
-                    print(message)
+                self.chat_box.insert(tk.END, message + '\n')
             except:
                 print('Error!')
                 self.client.close()
                 break
 
     def send_message(self):
-        while True:
-            message = f'{self.alias}: {input("")}'
-            self.client.send(message.encode('utf-8'))
+        message = f'{self.alias}: {self.message_entry.get()}'
+        self.client.send(message.encode('utf-8'))
+        self.message_entry.delete(0, tk.END)
+
+    def on_closing(self):
+        self.client.close()
+        self.root.destroy()
 
 if __name__ == "__main__":
     chat_client = ChatClient('127.0.0.1', 59000)
