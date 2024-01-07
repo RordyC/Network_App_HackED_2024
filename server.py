5376a0e290086553105e20eae6a5032dcc05cda0
Mohitsaroya <140790146+Mohitsaroya@users.noreply.github.com>
2024-01-06 17:33:38 -0700

    Update server.py
    made some slight changes in receive(self). this was done to make the text on the ui look a bit better

--- server.py
+++ server.py
@@ -35,14 +35,20 @@ class ChatServer:
             client, address = self.server.accept()
             print(f'Connection is established with {str(address)}')
             client.send('alias?'.encode('utf-8'))
-            alias = client.recv(1024)
+            alias = client.recv(1024).decode('utf-8')
             self.aliases.append(alias)
             self.clients.append(client)
-            print(f'The alias of this client is {alias}'.encode('utf-8'))
-            self.broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+            print(f'The alias of this client is {alias}')
+            
+            # Broadcast a cleaner connection message
+            connection_message = f" '{alias}' has connected to the chat room\n"
+            self.broadcast(connection_message.encode('utf-8'))
+
             client.send('You are now connected!'.encode('utf-8'))
-            thread = threading.Thread(target=self.handle_client, args=(client,))
+            thread = threading.Thread(target= self.handle_client, args=(client,))
             thread.start()
+        
+
 
 if __name__ == "__main__":
     chat_server = ChatServer('127.0.0.1', 59000)
